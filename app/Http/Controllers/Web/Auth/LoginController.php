8749fc12a4a439f731bb5de107e4ca9e5f63fce9
Ranjith Kumar <techlead@dubudubu.in>
2024-09-24 06:39:17 +0530

    commited

--- app/Http/Controllers/Web/Auth/LoginController.php
+++ app/Http/Controllers/Web/Auth/LoginController.php
@@ -79,7 +79,7 @@ class LoginController extends ApiController
     public function sessionstorage(Request $request){
         // dd($request['user_details']);
         $user_data = User::find($request['user_details']['id']);  
-        auth('web')->login($request['user_details']); 
+        // auth('web')->login($request['user_details']); 
         session(['user_details' => $request['user_details']]);
         $response = [
             "status"=>true,
